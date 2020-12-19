793b653e5fb44f488f204fd3178234b41b9ea339
ayline <ayline1695@hotmail.com>
2020-12-19 09:18:55 +0100

    no done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,14 +1,25 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.APIRequest = axios.create({
+      baseURL: baseUrl
+    })
   }
 
-  getFullList () {
-
+  async getFullList () {
+    try{
+      const {data : characters} = await this.APIRequest.get('/characters')
+      console.log(characters)
+      return characters
+    }catch(err){console.log(err)}
   }
 
-  getOneRegister () {
-
+  async getOneRegister () {
+    try{
+      const idCh = document.querySelector('.character-id');
+      const {data: dataId} = await this.APIRequest.get(`/characters/${idCh}`)
+      console.log("id's: ",dataId)
+      return dataId
+    }catch(err){console.error(err)}
   }
 
   createOneRegister () {
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,12 +1,24 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    event.preventDefault();
+    const charactersA = await charactersAPI.getFullList(); // esto es un objeto promesa (en espera)
+    const container = document.querySelector('.character-info');
+    container.innerHTML = ""
+    charactersA.forEach(characters => {
+      container.innerHTML += `${characters.name}`
+    });
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    event.preventDefault();
+    const idCharacters  = await charactersAPI.getOneRegister();
+    console.log("characters", idCharacters)
+    const container = document.querySelector('.character-info');
+    idCharacters.forEach(charactersid=> {
+      container.innerHTML += `${charactersid}`
+    })
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
